fbcffb0fb43676623850e8f83b08f2659053195e
Joyce Sheng <joycesheng96@hotmail.com>
2024-10-02 15:37:58 -0400

    saving index.js

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
 function addingEventListener() {
 }
+function addingEventListener() {
+    const input = document.getElementById('button');
+  
+    if (input) {
+      function clickAlert() {
+        alert('I was clicked!');
+      }
+      input.addEventListener('click', clickAlert);
+    }
+  }
+  
+  // Call the function to bind the event listener
+  addingEventListener();
+  
+  // Simulate a click event
+  let alertCalled = false;
+  window.alert = () => {
+    alertCalled = true;
+  };
+  
+  const input = document.getElementById('button');
+  input.click();
